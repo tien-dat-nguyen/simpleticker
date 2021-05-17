94ee83999e2d2ea496c204d823c0143a772b7697
dat.tien.nguyen <dat.tien.nguyen@grasshopperasia.com>
2021-05-17 14:32:06 +0700

    simple ticker: cpnvert async callbacks into coroutines

--- src/websocket-client.hpp
+++ src/websocket-client.hpp
@@ -24,12 +24,13 @@ namespace simpleticker
         const std::string& endpoint,
         const std::string& port,
         net::io_context& ioc,
-        net::ssl::context& ctx) : url_(url), endpoint_(endpoint), port_(port), resolver_(boost::asio::make_strand(ioc)), ws_(boost::asio::make_strand(ioc), ctx) 
+        net::ssl::context& ctx) : url_(url), endpoint_(endpoint), port_(port), 
+        /*resolver_(boost::asio::make_strand(ioc)),*/ ws_(boost::asio::make_strand(ioc), ctx) 
         { }
 
         ~websocket_client_t();
 
-        void Connect(
+        net::awaitable<void> Connect(
             std::function<void(error_code)> onConnect = nullptr,
             std::function<void(error_code,
                                std::string &&)>
@@ -40,12 +41,33 @@ namespace simpleticker
                 onMessage_ = onMessage;
                 onDisconnect_ = onDisconnect;
 
-                // Start the chain of asynchronous callbacks.
-                resolver_.async_resolve(url_, port_,
-                    [this](auto ec, auto endpoint) {
-                        OnResolve(ec, endpoint);
-                    });
+            // Start the chain of asynchronous callbacks.
+            // resolver_.async_resolve(url_, port_,
+            //     [this](auto ec, auto endpoint) {
+            //         OnResolve(ec, endpoint);
+            //     });
+
+            // use coroutine instead, avoid the callback hell above.
+            auto resolve_result = co_await ResolveAsync(url_, port_);
+            co_await ConnectTCPAsync(resolve_result);
+            co_await HandshakeTlsAsync();
+            co_await HandshakeWsAsync();
         }
+
+        net::awaitable<std::string> Consume() {
+            auto [ec, len] = co_await ReadAsync();
+            if (ec)
+            {
+                co_return std::string{};
+            }
+            else
+            {
+                std::string message {beast::buffers_to_string(rBuffer_.data())};
+                rBuffer_.consume(len);
+                co_return message;
+            }
+        }
+
 
         void Send(
             const std::string &message,
@@ -74,7 +96,7 @@ namespace simpleticker
         std::string endpoint_;
         std::string port_;
 
-        Resolver resolver_;
+        // Resolver resolver_;
         WebSocketStream ws_;
 
         beast::flat_buffer rBuffer_;
@@ -91,6 +113,15 @@ namespace simpleticker
                   << (ec ? "Error: " : "OK")
                   << (ec ? ec.message() : "")
                   << std::endl;
+        }
+
+        net::awaitable<net::ip::tcp::resolver::results_type> ResolveAsync(std::string const& host,
+            std::string const& port)
+        {
+            auto resolver =
+                Resolver{co_await net::this_coro::executor};
+            co_return co_await resolver.async_resolve(
+                host, port, net::use_awaitable);
         }
 
         void OnResolve(
@@ -112,6 +143,16 @@ namespace simpleticker
                 });
         }
 
+        net::awaitable<void> ConnectTCPAsync(net::ip::tcp::resolver::results_type results)
+        {
+            auto &tcp_layer = beast::get_lowest_layer(ws_);
+
+            tcp_layer.expires_after(std::chrono::seconds(5));
+
+            auto ep =  co_await tcp_layer.async_connect(results, net::use_awaitable);
+            boost::ignore_unused(ep);
+        }
+
         void OnConnect(const error_code &ec) {
             if (ec) {
                 Log("OnConnect", ec);
@@ -128,9 +169,19 @@ namespace simpleticker
 
         
             ws_.next_layer().async_handshake(net::ssl::stream_base::client,
-                [this](auto ec) {
-                    OnTlsHandshake(ec);
-                });
+                net::use_awaitable);
+        }
+
+        net::awaitable<void> HandshakeTlsAsync()
+        {
+            beast::get_lowest_layer(ws_).expires_never();
+            ws_.set_option(
+                beast::websocket::stream_base::timeout::suggested(
+                beast::role_type::client));
+
+        
+            co_await ws_.next_layer().async_handshake(net::ssl::stream_base::client,
+                net::use_awaitable);
         }
 
         void OnTlsHandshake(const error_code &ec) {
@@ -146,6 +197,11 @@ namespace simpleticker
                 [this](auto ec) {
                     OnHandshake(ec);
                 });
+        }
+
+        net::awaitable<void> HandshakeWsAsync() {
+            beast::websocket::response_type response;
+            co_await ws_.async_handshake(response, url_, endpoint_, net::use_awaitable);
         }
 
         void OnHandshake(
@@ -181,6 +237,12 @@ namespace simpleticker
                     OnRead(ec, nBytes);
                     ConsumeMessage(ec);
                 });
+        }
+
+        net::awaitable<std::pair<error_code, std::size_t>> ReadAsync() {
+            error_code ec;
+            auto size = co_await ws_.async_read(rBuffer_, net::redirect_error(net::use_awaitable, ec));
+            co_return std::make_pair(ec, size);
         }
 
         void OnRead(
